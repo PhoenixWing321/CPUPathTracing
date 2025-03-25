a266eb2d9400a123c634b54641f4288e309bb6dd
Fire Phoenix <3301647@qq.com>
2025-03-25 14:53:05 +0800

    export change to get value from env

--- export.ps1
+++ export.ps1
@@ -1,23 +1,17 @@
-# check if my.config exists  
-if (-not (Test-Path "my.config")) {
-    Write-Host "my.config is not found!"
+# Get paths from env
+if (-not $env:ROOT_DIR_EXPORT_LIB) {
+    # $env:ROOT_DIR_EXPORT_LIB = "E:\Root"
+    Write-Error "Environment variable ROOT_DIR_EXPORT_LIB is not set!"
     exit 1
 }
-
-# Read configuration file
-$config = Get-Content "my.config" | Where-Object { $_ -notmatch '^\s*#' -and $_ -notmatch '^\s*$' } | 
-ConvertFrom-StringData
-
-# Get paths from config
-$outputDir = $config.ROOT_DIR_EXPORT_LIB
-$buildDir = $config.ROOT_DIR_BUILD
-# $ROOT_DIR_3rdParty = $config.ROOT_DIR_3rdParty
+$outputDir = $env:ROOT_DIR_EXPORT_LIB
+$buildDir = "./build"
 
 # Copy files
-Write-Host "Copying Debug lib: $buildDir\source\Debug\CPUPathTracing.lib -> $outputDir\Debug\"   
-Copy-Item -Path "$buildDir\source\Debug\CPUPathTracing.lib" -Destination "$outputDir\Debug\" -Force
+Write-Host "Copying Debug lib: $buildDir\Debug\CPUPathTracing.lib -> $outputDir\debug\"   
+Copy-Item -Path "$buildDir\Debug\CPUPathTracing.lib" -Destination "$outputDir\debug\" -Force
 
-Write-Host "Copying Release lib: $buildDir\source\Release\CPUPathTracing.lib -> $outputDir\bin\"   
-    Copy-Item -Path "$buildDir\source\Release\CPUPathTracing.lib" -Destination "$outputDir\bin\" -Force
+Write-Host "Copying Release lib: $buildDir\Release\CPUPathTracing.lib -> $outputDir\bin\"   
+    Copy-Item -Path "$buildDir\Release\CPUPathTracing.lib" -Destination "$outputDir\bin\" -Force
 
 Write-Host "Export completed!"
